963461044034b05b64b9109d7bb5917ce76fb543
Vladislav <vladislav14141@yandex.ru>
2019-03-07 11:49:28 +1000

    Initial Commit

---
+++ Tab bar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tab bar
+//
+//  Created by Миронов Влад on 07/03/2019.
+//  Copyright © 2019 Миронов Влад. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
