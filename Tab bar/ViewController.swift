e7ff0e4246b764e6845d23464865fe679f2403b8
Vladislav <vladislav14141@yandex.ru>
2019-03-12 12:08:58 +1000

    Update

--- Tab bar/ViewController.swift
+++ Tab bar/ViewController.swift
@@ -9,12 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    var buttonName: String? {
+        didSet {
+            if button != nil {
+                button.setTitle(buttonName, for: .normal)
+            }
+        }
     }
-
-
+    
+    var colors: [UIColor] = [ .red, .orange, .lightGray, .blue]
+    
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let buttonName = buttonName {
+            button.setTitle(buttonName, for: .normal)
+        }
+    }
+    
+    func message(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        let fileName = file.components(separatedBy: "/").last ?? ""
+        
+        print("In \(fileName) line \(line) function \(function): \(message)")
+    }
+    
+    func buttonPressed(_ function: String = #function, color: UIColor) {
+        tabBarItem.title = function
+        
+        for viewController in tabBarController!.viewControllers! {
+            let mainViewController = viewController as! ViewController
+            mainViewController.view.backgroundColor = color
+            viewController.tabBarItem.badgeValue = nil
+        }
+        
+        tabBarItem.badgeColor = UIColor.red
+        tabBarItem.badgeValue = "!"
+    }
+    
+    
+    @IBAction func redButtonPressed(_ sender: UIButton) {
+        buttonPressed(color: .red)
+    }
+    
+    @IBAction func orangeButtonPressed(_ sender: UIButton) {
+        buttonPressed(color: .orange)
+    }
+    
+    @IBAction func cyanButtonPressed(_ sender: UIButton) {
+        buttonPressed(color: .cyan)
+    }
+    
+    @IBAction func grayButtonPressed(_ sender: UIButton) {
+        buttonPressed(color: .lightGray)
+    }
+    
+    
 }
 
